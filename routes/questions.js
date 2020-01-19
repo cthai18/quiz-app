5d5c4467fea679c5b2efef4b4f51cac8a5049c3c
Ches Thai <chesterthai@DNa1d6451.SUNet>
2020-01-19 14:27:08 -0800

    Add edit and delete routes for questions
    changed get route for questions to require a quizid

--- routes/questions.js
+++ routes/questions.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 let Question = require('../models/question.model');
 
 router.route('/').get((req, res) => {
-    console.log("Get request received for questions");
-    Question.find()
+    Question.find({
+            quizId: req.body.quizId
+        })
         .then(questions => res.json(questions))
         .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -18,7 +19,27 @@ router.route('/add').post((req, res) => {
 
     newQuestion.save()
         .then(question => res.json(question))
-        .catch(err => res.status(400).json('Error: ' + err))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/edit/:id').post((req, res) => {
+    Question.findById(req.params.id)
+        .then(question => {
+            question.description = req.body.description;
+            question.choices = req.body.choices;
+            question.correctAnswer = req.body.correctAnswer;
+
+            question.save()
+                .then(q => res.json(q))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Question.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Question Deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
