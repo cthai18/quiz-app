8bc2b7d9a863154854eae88e5d4c013815ed0d55
Ches Thai <chesterthai@DNa1d65ca.SUNet>
2020-01-22 21:35:54 -0800

    implemented creating a new quiz

--- frontend/src/components/QuizList.js
+++ frontend/src/components/QuizList.js
@@ -1,33 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import './styles/QuizList.css';
 import QuizListItem from './QuizListItem';
-import { Button, Spinner } from 'reactstrap';
+import { 
+    Button, 
+    Spinner, 
+    Modal, 
+    ModalHeader, 
+    ModalBody, 
+    ModalFooter, 
+    Form, 
+    FormGroup, 
+    Label, 
+    Input 
+} from 'reactstrap';
 import axios from 'axios';
 
-import { useHistory } from "react-router-dom";
-
-const QuizList = (props) => {
-    let history = useHistory();
-
+const QuizList = () => {
     const [quizzes, setQuizzes] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [modal, setModal] = useState(false);
+    const [newTitle, setNewTitle] = useState('');
 
     useEffect(() => {
+        fetchQuizzes();
+    }, []);
+
+    const toggle = () => {
+        setModal(!modal);
+    }
+
+    const fetchQuizzes = async () => {
         setLoading(true);
-        axios.get('/quizzes/')
-        .then(res => {
+        try {
+            const res = await axios.get('/quizzes/');
             console.log(res.data)
             setQuizzes(res.data);
             setLoading(false);
-        })
-        .catch(err => {
+            setNewTitle('');
+        } catch (err){
             setLoading(false);
             console.error(err);
-        });
-    }, []);
+        }
+    }
 
-    const onClickCreate = () => {
-        history.push('/create/');
+    const onChange = e => {
+        setNewTitle(e.target.value);
+    };
+
+    const onSubmit = e => {
+        console.log(newTitle);
+        axios.post('/quizzes/add', {
+            title: newTitle,
+            username: 'Alex' //TODO: CHANGE THIS HARDCODED NAME
+        })
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        toggle();
+        fetchQuizzes();
     };
 
     let listItems = (<Spinner size="lg" color="primary" />);
@@ -36,12 +69,26 @@ const QuizList = (props) => {
         <QuizListItem key={quiz._id} id={quiz._id} title={quiz.title} />);
     }
 
-
     return(
         <div className="d-flex container">
             <h1 className="title">My Quizzes</h1>
             {listItems}
-            <Button size="lg" color="primary" className="shadow-sm mt-3" onClick={onClickCreate}>Add New Quiz</Button>
+            <Button size="lg" color="primary" className="shadow-sm mt-3" onClick={toggle}>Add New Quiz</Button>
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Create New Quiz</ModalHeader>
+                <ModalBody>
+                    <Form onSubmit={onSubmit}>
+                        <FormGroup>
+                            <Label for="quizTitle">Title</Label>
+                            <Input type="text" name="quizTitle" id="quizTitle" value={newTitle} onChange={onChange}/>
+                        </FormGroup>
+                    </Form>
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={onSubmit}>Submit</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                </ModalFooter>
+            </Modal>
         </div>
     )
 }
